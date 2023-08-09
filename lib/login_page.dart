c1d6364cced6a47aae334572248623b223639758
Azbito <azbitomsc@gmail.com>
2023-08-09 18:25:02 -0300

    FEATURE: Errors login screen

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,6 +11,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
+  String emailError = '';
+  String passwordError = '';
 
   @override
   Widget build(BuildContext context) {
@@ -27,31 +29,54 @@ class _LoginPageState extends State<LoginPage> {
                         const SizedBox(height: 10),
                         TextField(
                           onChanged: (text) {
-                            email = text;
+                            setState(() {
+                              email = text;
+                              emailError = '';
+                            });
                           },
                           keyboardType: TextInputType.emailAddress,
                           decoration: const InputDecoration(
                               labelText: "Email", border: OutlineInputBorder()),
                         ),
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              emailError,
+                              textAlign: TextAlign.start,
+                              style: TextStyle(color: Colors.red.shade900),
+                            )),
                         const SizedBox(height: 10),
                         TextField(
                           onChanged: (text) {
                             password = text;
+                            passwordError = '';
                           },
                           obscureText: true,
                           decoration: const InputDecoration(
                               labelText: "Password",
                               border: OutlineInputBorder()),
                         ),
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              passwordError,
+                              textAlign: TextAlign.start,
+                              style: TextStyle(color: Colors.red.shade900),
+                            )),
                         const SizedBox(height: 10),
                         ElevatedButton(
                             onPressed: () {
-                              if (email.isNotEmpty && password.isNotEmpty) {
-                                Navigator.of(context).pushReplacement(
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const HomePage()));
+                              if (email.isEmpty) {
+                                emailError = 'Preencha o campo';
+                                return;
                               }
+                              if (password.isEmpty) {
+                                passwordError = 'Preencha o campo';
+                                return;
+                              }
+                              Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                      builder: (context) => const HomePage()));
                             },
                             child: const Text('Entrar'))
                       ],
