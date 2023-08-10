534504e1e2aee44f1e624b834675e8b82b3210fd
Azbito <azbitomsc@gmail.com>
2023-08-10 18:20:43 -0300

    FEATURE: Drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,6 +17,31 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+            child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+                currentAccountPicture: ClipRRect(
+                  borderRadius: BorderRadius.circular(40),
+                  child: Image.network(
+                      'https://media.discordapp.net/attachments/1098600696938516511/1139035996500009010/image.png'),
+                ),
+                accountName: const Text('Hunter'),
+                accountEmail: const Text('hunter@proton.me')),
+            const ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Início'),
+              subtitle: Text("Tela de início"),
+            ),
+            ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Logout'),
+                subtitle: const Text("Finalizar sessão"),
+                onTap: () {
+                  Navigator.of(context).pushReplacementNamed('/');
+                }),
+          ],
+        )),
         appBar: AppBar(
           title: const Text('Home Page', style: TextStyle(color: Colors.white)),
           actions: [ThemeSwitch()],
@@ -28,13 +53,6 @@ class HomePageState extends State<HomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(999),
-                    child: Image.network(
-                      'https://media.discordapp.net/attachments/1098600696938516511/1139035996500009010/image.png',
-                      width: 150,
-                    ),
-                  ),
                   Container(
                     padding: const EdgeInsets.all(30),
                     decoration: BoxDecoration(
